08f751f87183a3a502d8452d2c9fc5f259c93380
Lucas Araujo <lucasaraujo4188@gmail.com>
2022-10-21 11:00:26 -0300

    feat: Funcionalidades de cadastro, listagem e atualização de usuários implementadas

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -1,16 +1,21 @@
-import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class TurnUserAdminUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): void {
+        const receivedUser = this.usersRepository.findById(user_id);
+
+        if (!receivedUser) {
+            throw new Error("User doesn't exist!");
+        }
+
+        this.usersRepository.turnAdmin(receivedUser);
+    }
 }
 
 export { TurnUserAdminUseCase };
