3da05a0952ff04c87629cabc0d789b510b204b56
Lucas Araujo <lucasaraujo4188@gmail.com>
2022-10-25 10:22:56 -0300

    docs: adiciona documentação das rotas [PATCH]/users/:user_id/admin, [GET]/users/:user_id e [GET]/users

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -13,7 +13,7 @@ class TurnUserAdminController {
 
             return response.status(200).json(user);
         } catch (error) {
-            return response.status(404).json({ error: "Non-existing user" });
+            return response.status(404).json({ error: "User not found" });
         }
     }
 }
